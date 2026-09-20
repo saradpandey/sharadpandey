69cd48a5a4fb1bcd2a1e827195dba7b67c1c4f90
saradpandey <saradpandey2004@gmail.com>
2026-09-20 21:29:10 +0545

    Update all project files

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export default function Contact() {
     {
       icon: <FaPhoneAlt />,
       title: "Phone",
-      value: "+977 9709103714",
+      value: "+977 9749781949",
     },
     {
       icon: <FaMapMarkerAlt />,
